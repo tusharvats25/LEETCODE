077db0d6624e50bf5c6a58d7abbef4f274395adb
TUSHARVATSSINGH <130280113+tusharvats25@users.noreply.github.com>
2025-07-20 16:21:18 +0530

    Time: 3 ms (70.71%), Space: 14.9 MB (21.01%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,58 @@
+/* Brute force O(n^2)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
+*/
+
+//Using map O(N)
+/*
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        unordered_map<int,int>mp;
+        for (int i = 0; i < nums.size();i++) {
+            int complement = target - nums[i];
+            if (mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
+            }
+            mp[nums[i]] = i;
+        }
+        // If no solution is found, return an empty vector or handle error.
+        return {};
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) 
+    {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            int complement=target-nums[i];
+            if(mp.find(complement)!=mp.end())
+            return {i,mp[complement]};
+            mp[nums[i]]=i;
+        }
+        return {};
+    }
+};
