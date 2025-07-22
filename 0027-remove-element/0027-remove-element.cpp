6ac4b3ca5e29247045068d00366b008cf14b5e2b
TUSHARVATSSINGH <130280113+tusharvats25@users.noreply.github.com>
2025-07-22 14:17:11 +0530

    Time: 0 ms (100%), Space: 11.7 MB (41.24%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,11 @@
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        while(find(nums.begin(),nums.end(),val)!=nums.end()) 
+        {
+           nums.erase(remove(nums.begin(),nums.end(),val),nums.end());
+        }
+        return nums.size();
+    }
+};
