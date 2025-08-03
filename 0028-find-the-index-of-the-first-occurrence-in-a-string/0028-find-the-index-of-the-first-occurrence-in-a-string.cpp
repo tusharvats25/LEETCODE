e4de17256eb516c779f14cb1e5b20617b17a59b6
TUSHARVATSSINGH <130280113+tusharvats25@users.noreply.github.com>
2025-08-04 00:41:05 +0530

    Time: 0 ms (100%), Space: 8.7 MB (58.82%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -67,82 +67,89 @@
 
 
 //Brute force O(n*m)
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n=haystack.size(),m=needle.size();
+        for(int i=0;i<=n-m;i++)
+        {
+            int first=i,second=0;
+            while(second<m)
+            {
+                if(haystack[first]!=needle[second])
+                {
+                    break;
+                }
+                else
+                {
+                    first++,second++;
+                }
+            }
+            if(second==m)
+            {
+                return first-second;
+            }
+        }
+        return -1;
+    }
+};
+
+
+
+
+
+//KMP Best approach
 // class Solution {
 // public:
-//     int strStr(string haystack, string needle) {
-//         int n=haystack.size(),m=needle.size();
-//         for(int i=0;i<=n-m;i++)
+//     void lps_find(vector<int>&lps,string s)
+//     {
+//         int pre=0,suf=1;
+//         while(suf<s.size())
 //         {
-//             int first=i,second=0;
-//             while(second<m)
+//             if(s[pre]==s[suf])
 //             {
-//                 if(haystack[first]!=needle[second])
-//                 break;
+//                 lps[suf]=pre+1;
+//                 pre++,suf++;
+//             }
+//             else
+//             {
+//                 if(pre==0)
+//                 {
+//                     lps[suf]=0;
+//                     suf++;
+//                 }
 //                 else
+//                 {
+//                     pre=lps[pre-1];
+//                 }
+//             }
+//         }
+//     }
+//     int strStr(string haystack, string needle) {
+//         vector<int>lps(needle.size(),0);
+//         lps_find(lps,needle);
+//         int first=0,second=0;
+//         while(first<haystack.size() && second<needle.size())
+//         {
+//             if(haystack[first]==needle[second])
+//             {
 //                 first++,second++;
 //             }
-//             if(second==m)
-//             return first-second;
+//             else
+//             {
+//                 if(second==0)
+//                 {
+//                     first++;
+//                 }
+//                 else
+//                 {
+//                     second=lps[second-1];
+//                 }
+//             }
 //         }
+//         if(second==needle.size())
+//         return first-second;
 
 //         return -1;
 //     }
 // };
-
-//KMP Best approach
-
-
-class Solution {
-public:
-    void lps_find(vector<int>&lps,string s)
-    {
-        int pre=0,suf=1;
-        while(suf<s.size())
-        {
-            if(s[pre]==s[suf])
-            {
-                lps[suf]=pre+1;
-                pre++,suf++;
-            }
-            else
-            {
-                if(pre==0)
-                {
-                    lps[suf]=0;
-                    suf++;
-                }
-                else
-                {
-                    pre=lps[pre-1];
-                }
-            }
-        }
-    }
-    int strStr(string haystack, string needle) {
-        vector<int>lps(needle.size(),0);
-        lps_find(lps,needle);
-        int first=0,second=0;
-        while(first<haystack.size() && second<needle.size())
-        {
-            if(haystack[first]==needle[second])
-            {
-                first++,second++;
-            }
-            else
-            {
-                if(second==0)
-                {
-                    first++;
-                }
-                else
-                {
-                    second=lps[second-1];
-                }
-            }
-        }
-        if(second==needle.size())
-        return first-second;
-
-        return -1;
-    }
-};
