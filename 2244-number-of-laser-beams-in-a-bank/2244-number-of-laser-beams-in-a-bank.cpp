0790d9f546d65945f39b6321dbc9030f12b2d2b8
TUSHARVATSSINGH <130280113+tusharvats25@users.noreply.github.com>
2025-10-30 00:29:34 +0530

    Time: 13 ms (18.74%), Space: 27.2 MB (80.89%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        int start=0,ans=0;
+        int start=0;
+        int ans=0;
         for(auto& i : bank)
         {
             int curr=count(i.begin(),i.end(),'1');
@@ -14,3 +15,26 @@ public:
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     int numberOfBeams(vector<string>& bank) {
+//         int prev = 0, ans = 0;
+        
+//         for (string s : bank) {
+//             int count = 0;
+//             for (char c : s) {
+//                 if (c == '1') {
+//                     count++;
+//                 }
+//             }
+//             if (count != 0) {
+//                 ans += (prev * count);
+//                 prev = count;
+//             }
+//         }
+        
+//         return ans;
+//     }
+// };
