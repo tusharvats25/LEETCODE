f6d03316e531b76e6b5c6d9e9e9160be05268d9b
TUSHARVATSSINGH <130280113+tusharvats25@users.noreply.github.com>
2025-10-30 00:36:15 +0530

    Time: 12 ms (49.21%), Space: 45.6 MB (11.41%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.java
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.java
@@ -1,7 +1,7 @@
 class Solution {
   public int numberOfBeams(String[] bank) {
     int prev=0,ans=0;
-    for(var s: bank) {
+    for(var s: bank) {     //var==auto//
       int cnt=0;
       for(var i=0;i<s.length();i++)
         if(s.charAt(i)=='1') {
@@ -15,3 +15,25 @@ class Solution {
     return ans;
   }
 }
+
+
+// class Solution {
+//   public int numberOfBeams(String[] bank) {
+//     int prev = 0, ans = 0;
+
+//     for (String s: bank) {
+//       int count = 0;
+//       for (int i = 0; i < s.length(); i++)
+//         if (s.charAt(i) == '1') {
+//           count++;
+//         }
+
+//       if (count > 0) {
+//         ans += prev * count;
+//         prev = count;
+//       }
+//     }
+
+//     return ans;
+//   }
+// }
