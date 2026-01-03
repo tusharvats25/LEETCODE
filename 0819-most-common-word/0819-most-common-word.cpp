6f984c121944dcec8d5c2b1aa29f5ea74dd4a9e8
TUSHARVATSSINGH <130280113+tusharvats25@users.noreply.github.com>
2026-01-03 22:52:31 +0530

    Time: 2 ms (71.43%), Space: 10.2 MB (35.8%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+        unordered_set<string>ban(banned.begin(),banned.end());
+        unordered_map<string,int>mp;
+        string w,ans; 
+        int mx=0;
+        for(char c:paragraph+" ") {
+            if(isalpha(c)) w+=tolower(c);
+            else if(!w.empty()) {
+                if(!ban.count(w) && ++mp[w] > mx)
+                    mx=mp[w],ans=w;
+                w.clear();
+            }
+        }
+        return ans;
+    }
+};
